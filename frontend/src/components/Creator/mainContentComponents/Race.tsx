83d660d998ff6ba4643a4f400cf83d689fbdefbc
Simon Stenström <simon.stenstrom.013@gmail.com>
2025-01-09 15:40:30 +0100

    fixed skills for races

--- frontend/src/components/Creator/mainContentComponents/Race.tsx
+++ frontend/src/components/Creator/mainContentComponents/Race.tsx
@@ -3,19 +3,41 @@ import { useNewCharContext } from '../../../Context/CreatedCharacterContext';
 import { getDbObject } from '../../../functions/getDbItems';
 import { races } from '../../../database/dbRaces';
 import { IRace } from '../../../models/dbModels/IRace';
-import { INewCharacter } from '../../../models/INewCharater';
+import { INewCharacter, ISkillProfNewChar } from '../../../models/INewCharater';
 import { isActiveIcon } from '../../../functions/creatorMinorFunctions';
 
 export const Race = () => {
   const { newCharacter, setNewCharacter } = useNewCharContext();
   const [selectedRace, setSelectedRace] = useState(getDbObject(newCharacter.race, 'races'));
 
+  const changeRaceSkills = (changedRace: IRace) => {
+    let skills = newCharacter.skillProficiencies;
+    skills = skills.filter((o) => o.fromSource !== 'race');
+    if (changedRace.skillProficiencies) {
+      for (let i = 0; i < changedRace.skillProficiencies.length; i++) {
+        let skill: ISkillProfNewChar = {
+          id: changedRace.skillProficiencies[i],
+          fromSource: 'race',
+          canChange: false,
+        };
+        skills.push(skill);
+      }
+    }
+    return skills;
+  };
+
   const onChangeRace = (changedRace: IRace): void => {
     setSelectedRace(changedRace);
+    const newSkillProfs = changeRaceSkills(changedRace);
     if (changedRace.subraces) {
-      setNewCharacter({ ...newCharacter, race: changedRace.id, subrace: changedRace.subraces[0] });
+      setNewCharacter({
+        ...newCharacter,
+        race: changedRace.id,
+        subrace: changedRace.subraces[0],
+        skillProficiencies: newSkillProfs,
+      });
     } else {
-      const newState: INewCharacter = { ...newCharacter, race: changedRace.id };
+      const newState: INewCharacter = { ...newCharacter, race: changedRace.id, skillProficiencies: newSkillProfs };
       delete newState.subrace;
       setNewCharacter(newState);
     }
