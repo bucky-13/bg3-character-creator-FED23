db39ebe3fa36295ea89ee48be59cc6c37aef2d19
Simon Stenström <simon.stenstrom.013@gmail.com>
2025-01-24 14:33:28 +0100

    Added a nice little home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export const Home = () => {
-  return <p>This is home</p>;
+  return (
+    <div className="viewCharactersMainContainer">
+      <div className="viewCharactersContainer">
+        <h2>
+          Welcome to the <span className="siteLogoText">BG3 Character Creator</span>
+        </h2>
+        <p>
+          Here you can either <Link to={'/creator'}>create a new character</Link> for Baldur's Gate 3 or{' '}
+          <Link to={'/creator'}>view characters</Link> that others have created.
+        </p>
+        <p>At the moment you can only make a level 1 character.</p>
+      </div>
+    </div>
+  );
 };
