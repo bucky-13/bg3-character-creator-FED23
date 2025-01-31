7a496de6133e13c477718a72569571215378dd2c
Simon Stenström <simon.stenstrom.013@gmail.com>
2025-01-31 09:04:02 +0100

    fixed errant link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,9 @@ export const Home = () => {
         </h2>
         <p>
           Here you can either <Link to={'/creator'}>create a new character</Link> for Baldur's Gate 3 or{' '}
-          <Link to={'/creator'}>view characters</Link> that others have created.
+          <Link to={'/view'}>view characters</Link> that others have created.
         </p>
-        <p>At the moment you can only make a level 1 character.</p>
+        <p className="lastLineHome">At the moment you can make a character up to level 1.</p>
       </div>
     </div>
   );
