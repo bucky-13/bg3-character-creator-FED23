849a6bf286b2e0634befac9f272336fa470d861d
Simon Stenström <simon.stenstrom.013@gmail.com>
2025-01-31 09:23:45 +0100

    made home screen more fun

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,11 @@ export const Home = () => {
         <h2>
           Welcome to the <span className="siteLogoText">BG3 Character Creator</span>
         </h2>
+        <div className="homeScreenImagesContainer">
+          <img src="./images/Karlach.webp" alt="an image of a raging Karlach" />
+          <img src="./images/Astarion.webp" alt="an image of a sneaky Astarion" />
+        </div>
+
         <p>
           Here you can either <Link to={'/creator'}>create a new character</Link> for Baldur's Gate 3 or{' '}
           <Link to={'/view'}>view characters</Link> that others have created.
